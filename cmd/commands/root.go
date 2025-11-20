325d2c64e419ec268340f2453667330e24221501
Edvin N <edvin.norling@gmail.com>
2025-11-20 08:18:14 +0100

    Add version support to cli (#5)

--- cmd/commands/root.go
+++ cmd/commands/root.go
@@ -1,7 +1,16 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
+)
+
+// Version information set via ldflags
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
 )
 
 func NewCommand() *cobra.Command {
@@ -10,7 +19,11 @@ func NewCommand() *cobra.Command {
 		Short: "An Argo CD CLI offline utility",
 		Long: `A utility, based on Argo CD, that can be used "offline" (without requiring a running Argo CD server),
 to preview the Kubernetes resource manifests being created and managed by Argo CD.`,
+		Version: fmt.Sprintf("%s (commit: %s, built: %s)", version, commit, date),
 	}
+
+	// Enable -v as shorthand for --version
+	rootCmd.Flags().BoolP("version", "v", false, "version for argocd-offline-cli")
 
 	rootCmd.AddCommand(AppSetCommand())
 	rootCmd.AddCommand(AppCommand())
